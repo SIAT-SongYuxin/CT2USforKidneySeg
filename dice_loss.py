21ef4280c30232b8737bb6f06144d0f3472dff7b
SIAT-SongYuxin <55846725+SIAT-SongYuxin@users.noreply.github.com>
2021-05-22 12:58:20 +0800

    Add files via upload

---
+++ dice_loss.py
@@ -0,0 +1,192 @@
+from torch.autograd import Function
+import torch
+from torch import nn
+#from torch.autograd import Variable
+from torch import einsum
+import numpy as np
+
+
+class DiceCoeff(Function):
+    """Dice coeff for individual examples"""
+
+    def forward(self, input, target):
+        self.save_for_backward(input, target)
+        eps = 0.0001
+        self.inter = torch.dot(input.view(-1), target.view(-1))
+        self.union = torch.sum(input) + torch.sum(target) + eps
+
+        t = (2 * self.inter.float() + eps) / self.union.float()
+        return t
+
+    # This function has only a single output, so it gets only one gradient
+    def backward(self, grad_output):
+
+        input, target = self.saved_variables
+        grad_input = grad_target = None
+
+        if self.needs_input_grad[0]:
+            grad_input = grad_output * 2 * (target * self.union - self.inter) \
+                         / (self.union * self.union)
+        if self.needs_input_grad[1]:
+            grad_target = None
+
+        return grad_input, grad_target
+
+
+def dice_coeff(input, target):
+    """Dice coeff for batches"""
+    if input.is_cuda:
+        s = torch.FloatTensor(1).cuda().zero_()
+    else:
+        s = torch.FloatTensor(1).zero_()
+    score=[]
+    for i, c in enumerate(zip(input, target)):
+        #s = s + DiceCoeff().forward(c[0], c[1])
+        score.append(DiceCoeff().forward(c[0], c[1]).item())
+
+    return [np.mean(score),np.std(score,ddof=1)] #s / (i + 1)
+
+def sum_tensor(inp, axes, keepdim=False):
+    # copy from: https://github.com/MIC-DKFZ/nnUNet/blob/master/nnunet/utilities/tensor_utilities.py
+    axes = np.unique(axes).astype(int)
+    if keepdim:
+        for ax in axes:
+            inp = inp.sum(int(ax), keepdim=True)
+    else:
+        for ax in sorted(axes, reverse=True):
+            inp = inp.sum(int(ax))
+    return inp
+
+def get_tp_fp_fn(net_output, gt, axes=None, mask=None, square=False):
+    """
+    net_output must be (b, c, x, y(, z)))
+    gt must be a label map (shape (b, 1, x, y(, z)) OR shape (b, x, y(, z))) or one hot encoding (b, c, x, y(, z))
+    if mask is provided it must have shape (b, 1, x, y(, z)))
+    :param net_output:
+    :param gt:
+    :param axes:
+    :param mask: mask must be 1 for valid pixels and 0 for invalid pixels
+    :param square: if True then fp, tp and fn will be squared before summation
+    :return:
+    """
+    if axes is None:
+        axes = tuple(range(2, len(net_output.size())))
+
+    shp_x = net_output.shape
+    shp_y = gt.shape
+
+    with torch.no_grad():
+        if len(shp_x) != len(shp_y):
+            gt = gt.view((shp_y[0], 1, *shp_y[1:]))
+
+        if all([i == j for i, j in zip(net_output.shape, gt.shape)]):
+            # if this is the case then gt is probably already a one hot encoding
+            y_onehot = gt
+        else:
+            gt = gt.long()
+            y_onehot = torch.zeros(shp_x)
+            if net_output.device.type == "cuda":
+                y_onehot = y_onehot.cuda(net_output.device.index)
+            y_onehot.scatter_(1, gt, 1)
+
+    tp = net_output * y_onehot
+    fp = net_output * (1 - y_onehot)
+    fn = (1 - net_output) * y_onehot
+
+    if mask is not None:
+        tp = torch.stack(tuple(x_i * mask[:, 0] for x_i in torch.unbind(tp, dim=1)), dim=1)
+        fp = torch.stack(tuple(x_i * mask[:, 0] for x_i in torch.unbind(fp, dim=1)), dim=1)
+        fn = torch.stack(tuple(x_i * mask[:, 0] for x_i in torch.unbind(fn, dim=1)), dim=1)
+
+    if square:
+        tp = tp ** 2
+        fp = fp ** 2
+        fn = fn ** 2
+
+    tp = sum_tensor(tp, axes, keepdim=False)
+    fp = sum_tensor(fp, axes, keepdim=False)
+    fn = sum_tensor(fn, axes, keepdim=False)
+
+    return tp, fp, fn
+
+
+class IoULoss(nn.Module):
+    def __init__(self, apply_nonlin=None, batch_dice=False, do_bg=True, smooth=1.,
+                 square=False):
+        """
+        paper: https://link.springer.com/chapter/10.1007/978-3-319-50835-1_22
+
+        """
+        super(IoULoss, self).__init__()
+
+        self.square = square
+        self.do_bg = do_bg
+        self.batch_dice = batch_dice
+        self.apply_nonlin = apply_nonlin
+        self.smooth = smooth
+
+    def forward(self, x, y, loss_mask=None):
+        shp_x = x.shape
+
+        if self.batch_dice:
+            axes = [0] + list(range(2, len(shp_x)))
+        else:
+            axes = list(range(2, len(shp_x)))
+
+        if self.apply_nonlin is not None:
+            x = self.apply_nonlin(x)
+
+        tp, fp, fn = get_tp_fp_fn(x, y, axes, loss_mask, self.square)
+
+        iou = (tp + self.smooth) / (tp + fp + fn + self.smooth)
+
+        if not self.do_bg:
+            if self.batch_dice:
+                iou = iou[1:]
+            else:
+                iou = iou[:, 1:]
+        iou = iou.mean()
+
+        return iou
+
+class F1(nn.Module):
+    def __init__(self, apply_nonlin=None, batch_dice=False, do_bg=True, smooth=1.,
+                 square=False):
+        """
+        paper: https://link.springer.com/chapter/10.1007/978-3-319-50835-1_22
+
+        """
+        super(F1, self).__init__()
+
+        self.square = square
+        self.do_bg = do_bg
+        self.batch_dice = batch_dice
+        self.apply_nonlin = apply_nonlin
+        self.smooth = smooth
+
+    def forward(self, x, y, loss_mask=None):
+        shp_x = x.shape
+
+        if self.batch_dice:
+            axes = [0] + list(range(2, len(shp_x)))
+        else:
+            axes = list(range(2, len(shp_x)))
+
+        if self.apply_nonlin is not None:
+            x = self.apply_nonlin(x)
+
+        tp, fp, fn = get_tp_fp_fn(x, y, axes, loss_mask, self.square)
+
+        precision=tp/(tp+fp)
+        recall=tp/(tp+fn)
+        F1=2*precision*recall/(precision+recall)
+
+
+        if not self.do_bg:
+            if self.batch_dice:
+                F1 = F1[1:]
+            else:
+                F1 = F1[:, 1:]
+        F1 = F1.mean()
+
+        return F1
